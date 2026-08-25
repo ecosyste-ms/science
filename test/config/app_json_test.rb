fc39d22f1190cc7f2d8fe03effff364a755b9026
Andrew Nesbitt <andrewnez@gmail.com>
2026-08-25 14:47:48 +0100

    Schedule hourly Brief scans

---
+++ test/config/app_json_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class AppJsonTest < ActiveSupport::TestCase
+  test "Brief scans are scheduled hourly in bounded batches" do
+    config = JSON.parse(Rails.root.join("app.json").read)
+    brief_crons = config.fetch("cron").select do |cron|
+      cron.fetch("command").include?("projects:fetch_brief")
+    end
+
+    assert_equal [
+      {
+        "command" => "bundle exec rake projects:fetch_brief LIMIT=50",
+        "schedule" => "0 * * * *",
+      },
+    ], brief_crons
+  end
+end
