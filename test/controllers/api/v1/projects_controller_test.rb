d9a9473bd31eff398cc6f171f6682561b45c2c2d
Andrew Nesbitt <andrewnez@gmail.com>
2026-09-24 11:27:51 +0100

    Handle missing package metadata in funding links

--- test/controllers/api/v1/projects_controller_test.rb
+++ test/controllers/api/v1/projects_controller_test.rb
@@ -1,6 +1,31 @@
 require 'test_helper'
 
 class Api::V1::ProjectsControllerTest < ActionDispatch::IntegrationTest
+  test 'index includes funding links when some packages have no metadata' do
+    project = Project.create!(
+      url: 'https://github.com/example/funding-project',
+      last_synced_at: 1.hour.ago,
+      packages: [
+        { 'name' => 'missing-metadata' },
+        { 'name' => 'null-metadata', 'metadata' => nil },
+        { 'name' => 'empty-metadata', 'metadata' => {} },
+        { 'name' => 'string-funding', 'metadata' => { 'funding' => 'https://example.com/sponsor' } },
+        { 'name' => 'object-funding', 'metadata' => { 'funding' => { 'url' => 'https://example.com/donate' } } },
+        { 'name' => 'array-funding', 'metadata' => { 'funding' => ['https://example.com/support'] } }
+      ]
+    )
+
+    get api_v1_projects_path
+
+    assert_response :success
+    result = response.parsed_body.find { |entry| entry['id'] == project.id }
+    assert_equal [
+      'https://example.com/sponsor',
+      'https://example.com/donate',
+      'https://example.com/support'
+    ], result['funding_links']
+  end
+
   test "GET names returns unique package and project names" do
     Project.create!(
       url: 'https://github.com/test/project1',
